2c1f3c0d04fe5a95d4de22fab2acac9d729937f8
NeilPatilBooth <npatil@ChicagoBooth.edu>
2024-10-31 19:16:01 +0000

    homepage clean

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get ("/"), { :controller => "homepage", :action =>"home"}
+  get ("/", { :controller => "homepage", :action =>"home"})
 
   get("/rock", { :controller => "rock", :action => "play_rock" })
 
